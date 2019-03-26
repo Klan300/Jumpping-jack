e5fe9d45037219e278f52b7255da41ea82faa909
Klan300 <nathun.thun@hotmail.com>
2019-03-26 23:27:58 +0700

    change jumpping

--- coldetect.py
+++ coldetect.py
@@ -1,13 +1,25 @@
-def chack_player_platform_collsion(player_x,player_y,platform_x,platform_y):
+def check_player_platform_collsion(player_x,player_y,platform_x,platform_y):
     """
-    >>> chack_player_platform_collsion(60,60,90,24)
+    >>> check_player_platform_collsion(60,60,90,24)
     True
 
+    >>> check_player_platform_collsion(100,29,34,23)
+    False
+
     """
-    if platform_x - 36 <= player_x <= platform_x + 36 and player_y - 24  == platform_y + 12 :
+    if platform_x - 36 <= player_x + 3 <= platform_x + 36 and platform_y + 11 <= player_y - 24  <= platform_y + 12 :
         return True
     else:
         return False
+
+
+def test_case(sn,stn):
+    for i in range(sn,stn):
+        print(i)
+        if i == 3:
+            return i
+    else:
+        return 0
 
 
         
--- models.py
+++ models.py
@@ -1,51 +1,67 @@
 from random import randint
 import arcade.key
-from coldetect import chack_player_platform_collsion
+import arcade.physics_engines
+from coldetect import check_player_platform_collsion
 
 platform_center_y = 12
 platform_center_x = 35.5
 center_character_x = 25 + 3
 center_character_y = 24
 Gap_platform  = 35
-GRAVITY = -1
-MOVEMENT_SPEED = 2
 
+GRAVITY = 5
+MOVEMENT_SPEED = 5
 DIR_STILL = 0
 DIR_RIGHT = 1
 DIR_LEFT = 2
+DIR_UP = 3
 
 DIR_OFFSETS = {DIR_STILL: (0, 0),
                DIR_RIGHT: (1, 0),
-               DIR_LEFT: (-1, 0)}
+               DIR_LEFT: (-1, 0),
+               DIR_UP: (0,2)}
 
 KEY_MAP = {arcade.key.LEFT: DIR_LEFT,
-           arcade.key.RIGHT: DIR_RIGHT}
+           arcade.key.RIGHT: DIR_RIGHT,
+           arcade.key.SPACE: DIR_UP}
 
 class World:
+    COUNT_UP = 0
+
     def __init__(self,width,height):
 
         self.base_platform = Platform(self,width//2,platform_center_y)
-        self.character = Character(self,width//2-3,platform_center_y+center_character_y)
+        self.character = Character(self,width//2-3,platform_center_y*2+center_character_y)
         self.platform_list = Platform_list(self)
 
-
     def update(self,delta):
-        self.character.update(delta)
-
-        if self.platform_list.platform_checker(self.character):
-            self.character.y += 15
+        if check_player_platform_collsion(self.character.x,self.character.y,self.base_platform.x,self.base_platform.y):
+            GRAVITY = 0
+        
+        elif self.platform_list.platform_checker(self.character) == True:
+            GRAVITY = 0
         else:
-            self.character.y -= self.character.GRAVITY
+            GRAVITY = 1
+            self.character.y -= GRAVITY
 
+        self.character.update(delta)
+        
 
     def on_key_press(self, key, key_modifiers):
         if key in KEY_MAP:
-            self.character.direction = KEY_MAP[key]
-        
+            if key == arcade.key.SPACE :
+                self.COUNT_UP += 1
+                if self.COUNT_UP <= 3:
+                    self.character.direction = KEY_MAP[key]
+            else:
+                self.character.direction = KEY_MAP[key]
+                COUNT = 0        
+
     def on_key_relese(self, key, key_modifiers):
         if key in KEY_MAP:
             self.character.direction = DIR_STILL
 
+    
 
 
     
@@ -69,7 +85,7 @@ class Platform_list:
 
     def platform_checker(self,character):
         for platform in self.platform_now :
-            if chack_player_platform_collsion(character.x, character.y, platform.x, platform.y) == True:
+            if check_player_platform_collsion(character.x, character.y, platform.x, platform.y) == True:
                 return True
         else:
             return False
@@ -85,7 +101,6 @@ class Platform:
     
 
 class Character:
-    GRAVITY = -4
 
     def __init__(self, world,x,y):
         self.world = world
@@ -103,11 +118,6 @@ class Character:
         self.y += MOVEMENT_SPEED * DIR_OFFSETS[direction][1]
 
 
-    def character_touch_platform(character , platform_list):
-        if character.platform_checker(platform_list):
-            character.y += 15
-        else:
-            character.y -= Character.GRAVITY
 
                     
 
